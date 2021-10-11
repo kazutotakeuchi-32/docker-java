73f6c237f2a631d7bed7809ff6df20c9b725c984
kazutotakeuchi-32 <kazutotakeuchi32@gmail.com>
2021-10-11 23:40:26 +0900

    挙動確認

--- server/src/Main.java
+++ server/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        int num = 10 ;
+        String s = "abcdetc";
         System.out.println("Hello World!");
+	      System.out.println(1+1);
+        System.out.println(2+2);
+        System.out.println(num);
+        System.out.println(s);
     }
 }
